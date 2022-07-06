717818838243d4307744a32ecbfedce5a8f66ae9
JoeyGarber <joseph.z.garber@gmail.com>
2022-07-06 11:02:11 -0400

    Renamed header

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,16 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Joey's Portfolio`,
+    siteTitleAlt: `Joey Garber's Portfolio`,
+    siteHeadline: `Joey Garber's Portfolio`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
   },
+  pathPrefix: '/Portfolio',
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
